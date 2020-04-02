a9ff8ad68e9b4f8c53486cea71300832837f7b70
Ollie <olliejday@gmail.com>
2020-04-03 00:31:35 +0100

    working on AR Flows

--- homeworks/hw2/autoregressive_flow.py
+++ homeworks/hw2/autoregressive_flow.py
@@ -53,93 +53,146 @@ import numpy as np
 
 class MoGMLP(tf.keras.Model):
     """
-    MLP that outputs the mixture components, mean, stds of a mixture of gaussians
+    MLP that outputs the mean, stds of a mixture of gaussians
     with inputs as prev vars
     For first var, no prev vars to input to outputs are directly the parameters
     """
-    pass
+    def __init__(self, n_in, n_vals, n_units=128, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_in = n_in
+        self.n_units = n_units
+        self.n_vals = n_vals
+        self.n_out = self.n_vals * 2
+
+    def build(self, input_shape):
+        self.layers_list = []
+        if self.n_in > 0:
+            self.layers_list.append(tf.keras.layers.Dense(self.n_in))
+            self.layers_list.append(tf.keras.layers.Dense(self.n_units))
+            self.layers_list.append(tf.keras.layers.Dense(self.n_out))
+        else:
+            # if first layer fixed params, since no inputs
+            self.params = self.add_weight(name="Params", shape=(self.n_out,))
+
+    def call(self, inputs, training=None, mask=None):
+        if self.n_in > 0:
+            x = inputs
+            for layer in self.layers_list:
+                x = layer(x)
+        else:
+            # unconditional if first layer fixed params, since no inputs
+            x = tf.tile(self.params, [len(inputs)])
+        return tf.unstack(tf.reshape(x, (-1, self.n_vals, 2)), axis=-1)
 
 
-class MoG:
+class MoG(tf.keras.Model):
     """
     Mixture of Gaussians where mixture components, means and stddevs are compute by MLP with inputs of
     prev vars (no prev vars for first var)
     """
-    def __init__(self):
-        self.MoGMLP = MoGMLP()
-        self.trainable_weights = self.MoGMLP.trainable_weights
+    def __init__(self, n_in, n_out, k, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.k = k
+        self.components = [MoGMLP(n_in, n_out) for _ in range(self.k)]
+        self.mixture_weights = self.add_weight(name="MixureWeights", shape=(self.k,))
 
     def get_distributions(self, prev_x):
         dstrbns = []
-        for (weight, mean, std) in self.MoGMLP(prev_x):
-            dstbn = tfp.distributions.Normal(mean, std)
-            dstrbns.append((weight, dstbn))
+        for comp in self.components:
+            mean, std = comp(prev_x)
+            dstbn = tfp.distributions.Normal(tf.squeeze(mean), tf.squeeze(std))
+            dstrbns.append(dstbn)
         return dstrbns
 
+    def get_mixture_weights(self):
+        return tf.nn.softmax(self.mixture_weights)
+
     def pdf(self, x, prev_x):
-        prob_density = 0
-        for (weight, dstrbn) in self.get_distributions(prev_x):
-            prob_density += weight * dstrbn.prob(x)
-        return prob_density
+        weighted_sum, dstrbns, weights = self.setup_df(prev_x, x)
+        for i in range(self.k):
+            prob = dstrbns[i].prob(x)
+            prob = tf.where(tf.math.is_nan(prob), 0, prob)  # TODO: ok?
+            weighted_sum += weights[i] * prob
+        return weighted_sum
+
+    def concat_history(self, prev_x):
+        if prev_x is not None and len(prev_x) > 1:
+            prev_x = tf.concat(prev_x, axis=-1)
+        return prev_x
 
     def cdf(self, x, prev_x):
-        cum_density = 0
-        for (weight, dstrbn) in self.get_distributions(prev_x):
-            cum_density += weight * dstrbn.cdf(x)
-        return cum_density
+        weighted_sum, dstrbns, weights = self.setup_df(prev_x, x)
+        for i in range(self.k):
+            weighted_sum += weights[i] * dstrbns[i].cdf(x)
+        return weighted_sum
+
+    def setup_df(self, prev_x, x):
+        prev_x = self.concat_history(prev_x)
+        aggr = tf.zeros(tf.shape(x)[0])
+        dstrbns = self.get_distributions(prev_x)
+        weights = self.get_mixture_weights()
+        return aggr, dstrbns, weights
 
     def sample(self, n, z_sample, prev_x_sample):
+        # TODO: not using z_sample here??
+        _, dstrbns, weights = self.setup_df(prev_x_sample, x)
         # sample a distribution
-        dists_and_weights = self.get_distributions(prev_x_sample)
-        indx = tfp.distributions.Categorical(logits=[w for (_, w) in dists_and_weights]).sample()
-        dstrbn, _ = dists_and_weights[indx]
-        return dstrbn.sample(n)
+        indx = tfp.distributions.Categorical(logits=weights).sample(n)
+        samples = []
+        # sample as many samples from each distribution
+        for i in range(len(self.components)):
+            n_i = tf.reduce_sum(tf.where(indx == i), 1, 0)
+            samples += dstrbns[i].sample(n_i)
+        return samples
 
 
 class ARFlow:
-    def __init__(self, n_components):
-        self.n_components = n_components
-        # TODO: input prev component into next
-        self.components = [MoG() for _ in range(self.n_components)]
-        self.trainable_weights = [comp.trainable_weights for comp in self.components]
+    def __init__(self, n_vars, k=3):
+        self.n_vars = n_vars
+        # mixture per variable
+        self.mixtures = [MoG(i, 1, k) for i in range(self.n_vars)]
 
-    def log_p_x(self, x):
-        """
-        The log(p(f_{\theta}(x))) term is uniform
-        The log(det(d f(x) / d x)) term is the sum of the pdfs
-        :param x:
-        """
-        logpx = 0
-        for comp in self.components:
-            logpx += comp.pdf(x)
-        return logpx
+    # def log_p_x(self, inputs):
+    #     """
+    #     The log(p(f_{\theta}(x))) term is uniform
+    #     The log(det(d f(x) / d x)) term is the sum of the pdfs
+    #     """
+    #     logpx = 0
+    #     prev_x = []
+    #     for mix, x in zip(self.mixtures, inputs):
+    #         logpx += mix.pdf(x, prev_x)
+    #         prev_x.append(x)
+    #     return logpx
 
     def f(self, x):
         """
         Returns z = f_{\theta}(x)
         Numerically computed by mixture of CDFs of gaussians
         """
-        z = []
-        for comp in self.components:
-            z.append(comp.cdf(x))
-        return z
+        zs = []
+        for i in range(self.n_vars - 1):
+            z = self.mixtures[i].cdf(x[:, i], x[:, :i])
+            zs.append(z)
+        return zs
 
-    def sample(self, n):
-        """
-        Returns n samples
-        """
-        # sample z
-        z_sample = tfp.distributions.Uniform().sample((n, self.n_components))
-        x_sample = []
-        for comp in self.components:
-            x_sample.append(comp.sample(n, z_sample, x_sample))
-        return x_sample
-
-    def train(self, X):
-        with tf.GradientTape() as tape:
-            loss = self.log_p_x(X)
-        tape.gradient(loss, self.trainable_weights)
-
+    # def sample(self, n):
+    #     """
+    #     Returns n samples
+    #     """
+    #     # sample z
+    #     z_sample = tfp.distributions.Uniform().sample((n, self.n_vars))
+    #     x_sample = []
+    #     for mix in self.mixtures:
+    #         x_sample.append(mix.sample(n, z_sample, x_sample))
+    #     return x_sample
+    #
+    # def train(self, X):
+    #     with tf.GradientTape() as tape:
+    #         loss = self.log_p_x(X)
+    #     tape.gradient(loss, self.get_trainable_weights())
+    #
+    # def get_trainable_weights(self):
+    #     return [mix.get_trainable_weights() for mix in self.mixtures]
 
 
 def sample_data():
@@ -160,3 +213,21 @@ if __name__ == "__main__":
     # x, y = sample_data()
     # plt.plot(x[:, 0], x[:, 1], "x")
     # plt.show()
+
+    # TODO: seed
+
+    x = tf.cast(np.ones((10, 2)), tf.float32)
+    mog = MoG(0, 1, 5)
+    print(mog.pdf(x[:, 0], x[:, :0]))
+    print(mog.cdf(x[:, 0], x[:, :0]))
+    print(mog.sample(3, 0.1, x[:3, :0]))
+    mog = MoG(1, 1, 5)
+    print(mog.pdf(x[:, 1], x[:, :1]))
+    print(mog.cdf(x[:, 1], x[:, :1]))
+    print(mog.sample(3, 0.1, x[:3, :1]))
+
+    # flow = ARFlow(2)
+    # print(flow.f(x))
+    # print(flow.sample(1))
+    # print(flow.train(x))
+    # print(flow.log_p_x(x))
