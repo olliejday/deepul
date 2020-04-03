1b1ee005a0b0484e47e72fb2e0717568d5ed530b
Ollie <olliejday@gmail.com>
2020-04-03 12:14:05 +0100

    working on AR Flows

--- homeworks/hw2/autoregressive_flow.py
+++ homeworks/hw2/autoregressive_flow.py
@@ -51,7 +51,7 @@ import numpy as np
 
 #
 
-class MoGMLP(tf.keras.Model):
+class MoGMLP(tf.keras.layers.Layer):
     """
     MLP that outputs the mean, stds of a mixture of gaussians
     with inputs as prev vars
@@ -74,35 +74,45 @@ class MoGMLP(tf.keras.Model):
             # if first layer fixed params, since no inputs
             self.params = self.add_weight(name="Params", shape=(self.n_out,))
 
-    def call(self, inputs, training=None, mask=None):
+    def call(self, inputs, **kwargs):
         if self.n_in > 0:
             x = inputs
             for layer in self.layers_list:
                 x = layer(x)
         else:
-            # unconditional if first layer fixed params, since no inputs
-            x = tf.tile(self.params, [len(inputs)])
+            # unconditional if first layer fixed params
+            # since no inputs, input is batch size for number of distributions to have
+            x = tf.tile(self.params, tf.reshape(inputs, (1,)))
         return tf.unstack(tf.reshape(x, (-1, self.n_vals, 2)), axis=-1)
 
 
 class MoG(tf.keras.Model):
     """
-    Mixture of Gaussians where mixture components, means and stddevs are compute by MLP with inputs of
+    Mixture of Gaussians where mixture components, means apnd stddevs are compute by MLP with inputs of
     prev vars (no prev vars for first var)
     """
     def __init__(self, n_in, n_out, k, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.k = k
+        self.n_in = n_in
+        self.n_out = n_out
         self.components = [MoGMLP(n_in, n_out) for _ in range(self.k)]
         self.mixture_weights = self.add_weight(name="MixureWeights", shape=(self.k,))
 
-    def get_distributions(self, prev_x):
+    def get_distributions(self, prev_x, batch_size):
         dstrbns = []
         for comp in self.components:
-            mean, std = comp(prev_x)
-            dstbn = tfp.distributions.Normal(tf.squeeze(mean), tf.squeeze(std))
+            dstbn = self.get_distribution(comp, prev_x, batch_size)
             dstrbns.append(dstbn)
         return dstrbns
+
+    def get_distribution(self, comp, prev_x, batch_size):
+        # unconditional (1st var) has no inputs, so we input batch size for return shape
+        if self.n_in == 0:
+            prev_x = batch_size
+        mean, std = comp(prev_x)
+        dstbn = tfp.distributions.Normal(tf.squeeze(mean), tf.squeeze(std))
+        return dstbn
 
     def get_mixture_weights(self):
         return tf.nn.softmax(self.mixture_weights)
@@ -129,40 +139,50 @@ class MoG(tf.keras.Model):
     def setup_df(self, prev_x, x):
         prev_x = self.concat_history(prev_x)
         aggr = tf.zeros(tf.shape(x)[0])
-        dstrbns = self.get_distributions(prev_x)
+        dstrbns = self.get_distributions(prev_x, len(x))
         weights = self.get_mixture_weights()
         return aggr, dstrbns, weights
 
     def sample(self, n, z_sample, prev_x_sample):
         # TODO: not using z_sample here??
-        _, dstrbns, weights = self.setup_df(prev_x_sample, x)
+        weights = self.get_mixture_weights()
         # sample a distribution
-        indx = tfp.distributions.Categorical(logits=weights).sample(n)
+        indxs = tfp.distributions.Categorical(logits=weights).sample(n)
         samples = []
         # sample as many samples from each distribution
-        for i in range(len(self.components)):
-            n_i = tf.reduce_sum(tf.where(indx == i), 1, 0)
-            samples += dstrbns[i].sample(n_i)
+        for i, comp in enumerate(self.components):
+            # if we have samples from this component distribution
+            indx = tf.where(indxs == i)
+            n_i = len(indx)
+            if n_i > 0:
+                # get distribution for this component for each input
+                prev_x_i = None
+                if self.n_in > 0:
+                    prev_x_i = tf.gather(prev_x_sample, indx)
+                dstrbns = self.get_distribution(comp, prev_x_i, n_i)
+                # get a sample, the batch dimension is in the distribution by inputing a batch of inputs
+                sample = tf.reshape(dstrbns.sample(1), (n_i, 1))
+                samples.append(sample)
+        samples = tf.concat(samples, 0)
         return samples
 
 
 class ARFlow:
+    # TODO: atm assumes each var only sinlge 1D output
     def __init__(self, n_vars, k=3):
         self.n_vars = n_vars
         # mixture per variable
         self.mixtures = [MoG(i, 1, k) for i in range(self.n_vars)]
 
-    # def log_p_x(self, inputs):
-    #     """
-    #     The log(p(f_{\theta}(x))) term is uniform
-    #     The log(det(d f(x) / d x)) term is the sum of the pdfs
-    #     """
-    #     logpx = 0
-    #     prev_x = []
-    #     for mix, x in zip(self.mixtures, inputs):
-    #         logpx += mix.pdf(x, prev_x)
-    #         prev_x.append(x)
-    #     return logpx
+    def log_p_x(self, inputs):
+        """
+        The log(p(f_{\theta}(x))) term is uniform
+        The log(det(d f(x) / d x)) term is the sum of the pdfs
+        """
+        logpx = 0
+        for i in range(self.n_vars - 1):
+            logpx += self.mixtures[i].pdf(x[:, i], x[:, :i])
+        return logpx
 
     def f(self, x):
         """
@@ -175,24 +195,32 @@ class ARFlow:
             zs.append(z)
         return zs
 
-    # def sample(self, n):
-    #     """
-    #     Returns n samples
-    #     """
-    #     # sample z
-    #     z_sample = tfp.distributions.Uniform().sample((n, self.n_vars))
-    #     x_sample = []
-    #     for mix in self.mixtures:
-    #         x_sample.append(mix.sample(n, z_sample, x_sample))
-    #     return x_sample
-    #
-    # def train(self, X):
-    #     with tf.GradientTape() as tape:
-    #         loss = self.log_p_x(X)
-    #     tape.gradient(loss, self.get_trainable_weights())
-    #
-    # def get_trainable_weights(self):
-    #     return [mix.get_trainable_weights() for mix in self.mixtures]
+    def sample(self, n):
+        """
+        Returns n samples
+        """
+        # sample z
+        z_sample = tfp.distributions.Uniform().sample((n, self.n_vars))
+        x_samples = []
+        x_sample = None
+        for mix in self.mixtures:
+            if len(x_samples) > 0:
+                x_sample = tf.concat(x_samples, -1)
+            x_samples.append(mix.sample(n, z_sample, x_sample))
+        x_sample = tf.concat(x_samples, -1)
+        return x_sample
+
+    def train(self, X):
+        with tf.GradientTape() as tape:
+            loss = self.loss(X)
+        tape.gradient(loss, self.get_trainable_weights())
+        return loss
+
+    def loss(self, X):
+        return tf.reduce_mean(self.log_p_x(X))
+
+    def get_trainable_weights(self):
+        return [mix.trainable_weights for mix in self.mixtures]
 
 
 def sample_data():
@@ -210,24 +238,23 @@ def sample_data():
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    # x, y = sample_data()
+    x, y = sample_data()
     # plt.plot(x[:, 0], x[:, 1], "x")
     # plt.show()
 
     # TODO: seed
 
-    x = tf.cast(np.ones((10, 2)), tf.float32)
-    mog = MoG(0, 1, 5)
-    print(mog.pdf(x[:, 0], x[:, :0]))
-    print(mog.cdf(x[:, 0], x[:, :0]))
-    print(mog.sample(3, 0.1, x[:3, :0]))
-    mog = MoG(1, 1, 5)
-    print(mog.pdf(x[:, 1], x[:, :1]))
-    print(mog.cdf(x[:, 1], x[:, :1]))
-    print(mog.sample(3, 0.1, x[:3, :1]))
+    # mog = MoG(0, 1, 5)
+    # print(mog.pdf(x[:, 0], x[:, :0]))
+    # print(mog.cdf(x[:, 0], x[:, :0]))
+    # print(mog.sample(3, 0.1, x[:3, :0]))
+    # mog = MoG(1, 1, 5)
+    # print(mog.pdf(x[:, 1], x[:, :1]))
+    # print(mog.cdf(x[:, 1], x[:, :1]))
+    # print(mog.sample(3, 0.1, x[:3, :1]))
 
-    # flow = ARFlow(2)
-    # print(flow.f(x))
-    # print(flow.sample(1))
-    # print(flow.train(x))
-    # print(flow.log_p_x(x))
+    flow = ARFlow(2)
+    print(flow.f(x))
+    print(flow.log_p_x(x))
+    print(flow.train(x))
+    print(flow.sample(2))
