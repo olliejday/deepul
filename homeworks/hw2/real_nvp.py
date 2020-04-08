8c705a0061831340ef25dc2cb5199b04c9392b78
Ollie <olliejday@gmail.com>
2020-04-08 15:51:54 +0100

    working on realnvp

--- homeworks/hw2/real_nvp.py
+++ homeworks/hw2/real_nvp.py
@@ -6,42 +6,24 @@ from autoregressive_flow import sample_data, DenseNN
 
 
 class RealNVP:
+    """
+    2 variable, MLP version of RealNVP
+    """
     def __init__(self):
         self.model = self.setup_model()
 
     def setup_model(self):
-        pass
+        return RealNVPModel()
 
     def train(self, x):
         pass
 
 
 class RealNVPModel(tf.keras.Model):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def build(self, input_shape):
-        self.affine_transf_flow = AffineTransformationFlow()
-
-    def f_x(self, inputs):
-        """
-        z = f(x)
-        :param inputs: (bs, 2) Xs inputs
-        :return: (bs, 2) Zs output
-        """
-        inputs_split = tf.split(inputs, 2, -1)
-        return tf.stack(self.affine_transf_flow(inputs_split), -1)
-
-    def log_p_x(self, inputs):
-        """
-        computes log p(x) =   log p(f(x)) + log(det( df(x) / dx))
-        :param inputs: (bs, 2) Xs inputs
-        :return: (bs,) probs output
-        """
-        pass
-
-
-class AffineTransformationFlow(tf.keras.layers.Layer):
+    """
+    Model for RealNVP flow
+    For 2 variables using MLPs (FF-NN)
+    """
     def __init__(self, n_units=128, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
         super().__init__(trainable, name, dtype, dynamic, **kwargs)
         self.n_units = n_units
@@ -53,17 +35,36 @@ class AffineTransformationFlow(tf.keras.layers.Layer):
         self.g_theta_shift = DenseNN(self.n_units, 1, activation="tanh")
         self.scale = self.add_weight("scale", shape=(1,))
         self.scale_shift = self.add_weight("scale_shift", shape=(1,))
+        # for z prior (2 variables)
+        self.z_prior = tfp.distributions.Normal([0, 0], [1, 1])
 
-    def call(self, inputs, **kwargs):
+    def f_x(self, inputs):
         """
-        :param inputs: ((bs,), (bs,)) tuple of x1 and x2 inputs
-        :return: ((bs,), (bs,)) tuple of z1 and z2 outputs
+        computes z = f(x)
+        :param inputs: (bs, 2) Xs inputs
+        :return: (bs, 2) Zs output
         """
-        x1, x2 = inputs
+        x1, x2 = tf.split(inputs, 2, -1)
         z1 = x1
         log_scale = self.scale * tf.tanh(self.g_theta_scale(x1)) + self.scale_shift
         z2 = tf.exp(log_scale) * x2 + self.g_theta_shift(x1)
-        return z1, z2
+        return tf.stack([z1, z2], -1)
+
+    def log_p_x(self, inputs):
+        """
+        computes log p(x) =   log p(f(x)) + log(det( df(x) / dx))
+        :param inputs: (bs, 2) Xs inputs
+        :return: (bs,) probs output
+        """
+        # z prior prob
+        log_p_z = self.z_prior.log_prob(self.f_x(inputs))
+        x1, _ = inputs
+        log_scale = self.scale * tf.tanh(self.g_theta_scale(x1)) + self.scale_shift
+        # since jac is triangular, det of jac is prod of diag
+        # but in this 2 variable case the first diagonal is 1
+        df_dx = tf.exp(log_scale)
+        log_det_jac = tf.math.log(df_dx)
+        return log_p_z + log_det_jac
 
 
 if __name__ == "__main__":
