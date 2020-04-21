a00ef02674915712ec14e19a44ca1814ad4fb97e
Ollie <olliejday@gmail.com>
2020-04-21 23:06:54 +0100

    working on pixelCNN AR Flow

--- homeworks/hw2/ar_flow_images.py
+++ homeworks/hw2/ar_flow_images.py
@@ -3,9 +3,9 @@ import tensorflow_probability as tfp
 import numpy as np
 from autoregressive_flow import ARFlow
 
-# TODO: batch norm?
+# TODO: improve performance -  bug?
 # TODO: nan loss
-# TODO: neg loss?
+# TODO: neg loss
 # TODO: dequantize the data and scale, account for in loss scaling
 
 class PixelCNNARFlow(ARFlow):
@@ -302,7 +302,7 @@ if __name__ == "__main__":
     x = x + np.random.random((bs, H, W, C))
     # scale to [0, 1]
     x = x / np.max(x)
-    for i in range(50):
+    for i in range(20):
         print(model.train(x))
     samples = model.sample(3)
     samples = np.squeeze(samples)
